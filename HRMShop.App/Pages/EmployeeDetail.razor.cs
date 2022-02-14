baf88135ef326a2692461d57f7572fbd19981819
Amr Elshaer <amrelsher07@gmail.com>
2022-02-14 21:29:59 +0200

    Add project files.

---
+++ HRMShop.App/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,62 @@
+﻿using HRMShop.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMShop.App.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public string EmployeeId { get; set; }
+        public Employee Employee { get; set; } = new();
+        public IEnumerable<Employee> Employees { get; set; }
+        public List<Country> Countries { get; set; }
+        public List<JobCategory> JobCategories { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            InitCountries();
+            InitJobCategories();
+            InitEmployees();
+            Employee= Employees.FirstOrDefault(e=>e.EmployeeId==int.Parse(EmployeeId));
+            return base.OnInitializedAsync();
+        }
+
+        private void InitCountries()
+        {
+            Countries = new List<Country>() {
+              new Country()
+              {
+                  CountryId=1,
+                  Name="Egypt"
+              }
+            };
+
+        }
+
+        private void InitJobCategories()
+        {
+            JobCategories = new List<JobCategory>()
+            {
+                new JobCategory{JobCategoryId=1,JobCategoryName="Job"}
+            };
+        }
+
+        private void InitEmployees()
+        {
+            Employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    CountryId=1,
+                    EmployeeId=1,
+                    JobCategoryId=1,
+                    FirstName="Amr",
+                    LastName="Elshaer"
+                }
+            };
+        }
+    }
+}
---
+++ HRMShop.App/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,57 @@
+﻿using HRMShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRMShop.App.Pages
+{
+    public partial class EmployeeOverview
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        public List<Country> Countries { get; set; }
+        public List<JobCategory> JobCategories { get; set; }
+        protected override Task OnInitializedAsync()
+        {
+            InitCountries();
+            InitJobCategories();
+            InitEmployees();
+            return base.OnInitializedAsync();
+        }
+
+        private void InitCountries()
+        {
+            Countries = new List<Country>() { 
+              new Country()
+              {
+                  CountryId=1,
+                  Name="Egypt"
+              }
+            };
+
+        }
+
+        private void InitJobCategories()
+        {
+            JobCategories = new List<JobCategory>()
+            {
+                new JobCategory{JobCategoryId=1,JobCategoryName="Job"}
+            };
+        }
+
+        private void InitEmployees()
+        {
+            Employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    CountryId=1,
+                    EmployeeId=1,
+                    JobCategoryId=1,
+                    FirstName="Amr",
+                    LastName="Elshaer"
+                }
+            };
+        }
+    }
+}
